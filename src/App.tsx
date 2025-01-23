4711d9f6dbaf21ce3b4f4a8bcdfad3cb0959f7f0
juditvarro <dr.judit.varro@gmail.com>
2025-01-23 19:22:52 +0100

    feat: handle active state of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,16 @@ function App() {
             intro="Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly."
           >
             <div className="flex flex-row justify-center gap-x-4 my-4">
-              <Button text="All Products" handleClick={handleAllClick} />
-              <Button text="Available Now" handleClick={handleAvailableClick} />
+              <Button
+                text="All Products"
+                selected={!filter}
+                handleClick={handleAllClick}
+              />
+              <Button
+                text="Available Now"
+                selected={filter}
+                handleClick={handleAvailableClick}
+              />
             </div>
             <div className="my-8 px-6">
               {source && <CoffeeList source={source} filter={filter} />}
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 interface ButtonProps {
   text: string;
+  selected: boolean;
   handleClick: (clicked: boolean) => void;
 }
 
-const Button = ({ text, handleClick }: ButtonProps) => {
+const Button = ({ text, selected, handleClick }: ButtonProps) => {
   return (
     <button
       onClick={() => handleClick(true)}
-      className="text-themeLightestGray font-dm font-light text-[14px] p-2 rounded-md hover:bg-themeDarkGray "
+      className={`text-themeLightestGray font-dm font-light text-[14px] p-2 rounded-md ${selected ? 'bg-themeDarkGray' : 'bg-inherit'}`}
     >
       {text}
     </button>
